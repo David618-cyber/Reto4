48dea3165aa8425c4dbc0819e0d4b9f6a9ee5208
DAM1 <DAM1@aula11-pc18>
2024-11-19 11:40:20 +0100

    Funciones peso

--- Reto4/src/claseMain.java
+++ Reto4/src/claseMain.java
@@ -2,7 +2,7 @@
 public class claseMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	     
 
 	}
 
--- Reto4/src/funciones2.java
+++ Reto4/src/funciones2.java
@@ -1,4 +1,9 @@
 
 public class funciones2 {
-
+    public static double KilosLibras(int peso) {
+    	return peso*2.205;
+    }
+    public static double LibrasKilos(int peso) {
+    	return peso/2.205;
+    }
 }
