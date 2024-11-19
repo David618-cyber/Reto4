d8873d573a2072d5c557c484beaf8947d137422b
david618-cyber <davidjimeno077@gmail.com>
2024-11-19 11:54:05 +0100

    hecho main

--- Reto4/src/claseMain.java
+++ Reto4/src/claseMain.java
@@ -4,30 +4,32 @@ import Reto2.Funciones2;
 public class claseMain {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		int resultado = 0;
-		int n1 = 0;
+		double n1 = 0;
+		double n2=0;
 		do {
 			System.out.println("Selecciona tipo de conversión :");
 			System.out.println("1. longuitud");
 			System.out.println("2. Peso");
+			System.out.println("0. Peso");
 			switch (resultado) {
-			case 1:
+			case 2:
 				System.out.println(
 						"El resultado de conversión de longuitud  de millas a kilometros es :" + Funciones1.m(n1));
 				System.out.println("El resultado de conversión de  kilometros a millas es :" + Funciones1.k(n1));
 			case 2:
 				System.out.println(
-						"El resultado de conversión de  kilogramos a libras es :" + funciones2.kilosLibras(n1));
+						"El resultado de conversión de  kilogramos a libras es :" + funciones1.KL(n2));
+			case 3:
 				System.out.println(
-						"El resultado de conversión de  kilogramos a libras es :" + funciones2.LibrasKilos(n1));
+						"El resultado de conversión de  kilogramos a libras es :" + funciones2.LibrasKilos(n2));
+				
 
 			}
 
-		} while (!(resultado >= 0 && resultado < 2));
-=======
-	     
->>>>>>> branch 'main' of https://github.com/David618-cyber/Reto4.git
+		} while (!(resultado >= 0 && resultado < 4));
+
 
 	}
 
