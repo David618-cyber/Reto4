de6df76c52dc60902de4a931de5e1d02cd063a94
DAM1 <DAM1@aula11-pc18>
2024-11-19 11:50:06 +0100

    modificacion funciones2

--- Reto4/src/funciones2.java
+++ Reto4/src/funciones2.java
@@ -1,9 +1,9 @@
 
 public class funciones2 {
-    public static double KilosLibras(int peso) {
+    public static double KL(int peso) {//de kilos a libras
     	return peso*2.205;
     }
-    public static double LibrasKilos(int peso) {
+    public static double LibrasKilos(int peso) {//de libras a kilos
     	return peso/2.2050;
     }
 }
