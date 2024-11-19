0b06b48babf520f10649c623514d4ee7f7a83dfd
david618-cyber <davidjimeno077@gmail.com>
2024-11-19 11:33:10 +0100

    añadido funciones 2

--- Reto4/src/Funciones1.java
+++ Reto4/src/Funciones1.java
@@ -1,9 +1,10 @@
 
 public class Funciones1 {
 
+	public static double m(double millas) { // millas a kilometros//
+		return millas * 1.609;
+
+	}
 	
-	public static double m(double millas) { //millas a kilometros//
-		return millas*1.6090;
 	
-}
 }
